e81fccc363b43b5474c11b53d93862a85ee57735
Biswajit Paul <gokulato009@gmail.com>
2026-08-31 20:32:31 +0530

    Time: 762 ms (41.39%), Space: 12.9 MB (95.21%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,3 +4,11 @@ class Solution(object):
             n=target-nums[i]
             if n in nums and nums.index(n)!=i:
                 return [i,nums.index(n)]
+
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)-1):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return i,j
